abc7b3ca398f95e4be9aa48d01e71503f93f674e
ppkill00 <ppkill00@gmail.com>
2015-10-29 14:35:50 +0900

    new func

--- d_autoconfig.sh
+++ d_autoconfig.sh
@@ -3,12 +3,36 @@
 # Description : this is autoconfig for my env
 
 
-sudo add-apt-repository ppa:numix/ppa
-sudo aptitude update
+#sudo add-apt-repository ppa:numix/ppa
+#sudo aptitude update
 
-sudo aptitude install php5 mysql-server mysql-client php5-mysql vsftpd apache2 nmap chromium-browser terminator ibus-hangul gir1.2-gtop-2.0 vim ssh numix-gtk-theme numix-icon-theme numix-icon-theme-circle gnome-tweak-tool sysv-rc-conf git-core python-pip
+#sudo aptitude install php5 mysql-server mysql-client php5-mysql vsftpd apache2 nmap chromium-browser terminator ibus-hangul gir1.2-gtop-2.0 vim ssh numix-gtk-theme numix-icon-theme numix-icon-theme-circle gnome-tweak-tool sysv-rc-conf git-core python-pip
 
-echo "*=========vimrc setup=========*"
+
+echo "  _________________________________________________________________________  "
+echo " /                            __ __  ____ ______ ____   ____               \ "
+echo " |                           / //_/ /  _//_  __// __ \ /  _/               | "
+echo " |                          / ,<    / /   / /  / /_/ / / /                 | "
+echo " |                         / /| | _/ /   / /  / _, _/_/ /                  | "
+echo " |                        /_/ |_|/___/  /_/  /_/ |_|/___/                  | "
+echo " |                                                                         | "
+echo " |             ___           __                              ____ _        | "
+echo " |            /   |  __  __ / /_ ____   _____ ____   ____   / __/(_)____ _ | "
+echo " |           / /| | / / / // __// __ \ / ___// __ \ / __ \ / /_ / // __ \`/ | "
+echo " |          / ___ |/ /_/ // /_ / /_/ // /__ / /_/ // / / // __// // /_/ /  | "
+echo " |         /_/  |_|\__,_/ \__/ \____/ \___/ \____//_/ /_//_/  /_/ \__, /   | "
+echo " \                                                               /____/    / "
+echo "  -------------------------------------------------------------------------  "
+echo "   \                                                                         "
+echo "    \   \_\_    _/_/                                                         "
+echo "     \      \__/                                                             "
+echo "            (oo)\_______                                                     "
+echo "            (__)\       )\/\                                                 "
+echo "                ||----w |                                                    "
+echo "                ||     ||                                                    "
+
+
+echo "*=========  vimrc     setup  =========*"
 if [ -f /etc/vim/vimrc ]
 then
   sudo cp ./vimrc /etc/vim/vimrc
@@ -18,5 +42,5 @@ else
   read virc
   suco cp ./vimrc $virc
 fi
-echo "*=========vimrc setup end=========*"
+echo "*=========  vimrc setup end  =========*"
 
--- s_autoconfig.sh
+++ s_autoconfig.sh
@@ -8,6 +8,8 @@ sudo aptitude update
 sudo aptitude install php5 mysql-server mysql-client php5-mysql vsftpd apache2 nmap vim ssh  sysv-rc-conf git-core python-pip
 
 echo "*=========vimrc setup=========*"
+
+
 if [ -f /etc/vim/vimrc ]
 then
   sudo cp ./vimrc /etc/vim/vimrc
