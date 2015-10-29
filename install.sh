4af1b4810b1cfb9eab60d55eb25e79c0bb78896c
ppkill00 <ppkill00@gmail.com>
2015-10-29 15:14:20 +0900

    fix

--- install.sh
+++ install.sh
@@ -3,33 +3,49 @@
 # Description : this is autoconfig for my env
 
 
+
+
+echo " _________________________________________________________________________  "
+echo "/             ___           __                              ____ _        \ "
+echo "|            /   |  __  __ / /_ ____   _____ ____   ____   / __/(_)____ _ | "
+echo "|           / /| | / / / // __// __ \ / ___// __ \ / __ \ / /_ / // __ \`/ | "
+echo "|          / ___ |/ /_/ // /_ / /_/ // /__ / /_/ // / / // __// // /_/ /  | "
+echo "|         /_/  |_|\__,_/ \__/ \____/ \___/ \____//_/ /_//_/  /_/ \__, /   | "
+echo "\                                                               /____/    / "
+echo " -------------------------------------------------------------------------  "
+echo "  \                                                                         "
+echo "   \   \_\_    _/_/                                                         "
+echo "    \      \__/                                                             "
+echo "           (oo)\_______                                                     "
+echo "           (__)\       )\/\                                                 "
+echo "               ||----w |                                                    "
+echo "               ||     ||                                                    "
+
+
+echo "Select your usage. [Desktop/Server]"
+read system
+case $system in 
+  "server")
+    echo "select Server"
+    sudo aptitude update
+    sudo aptitude install php5 mysql-server mysql-client php5-mysql vsftpd apache2 nmap vim ssh  sysv-rc-conf git-core python-pip
+    ;;
+  "desktop")
+    echo "select Desktop"
+    sudo add-apt-repository ppa:numix/ppa
+    sudo aptitude update
+    sudo aptitude install php5 mysql-server mysql-client php5-mysql vsftpd apache2 nmap chromium-browser terminator ibus-hangul gir1.2-gtop-2.0 vim ssh numix-gtk-theme numix-icon-theme numix-icon-theme-circle gnome-tweak-tool sysv-rc-conf git-core python-pip
+    ;;
+  *)
+    echo "??"
+    ;;
+esac
+
 #sudo add-apt-repository ppa:numix/ppa
 #sudo aptitude update
 
 #sudo aptitude install php5 mysql-server mysql-client php5-mysql vsftpd apache2 nmap chromium-browser terminator ibus-hangul gir1.2-gtop-2.0 vim ssh numix-gtk-theme numix-icon-theme numix-icon-theme-circle gnome-tweak-tool sysv-rc-conf git-core python-pip
 
-
-echo "  _________________________________________________________________________  "
-echo " /                            __ __  ____ ______ ____   ____               \ "
-echo " |                           / //_/ /  _//_  __// __ \ /  _/               | "
-echo " |                          / ,<    / /   / /  / /_/ / / /                 | "
-echo " |                         / /| | _/ /   / /  / _, _/_/ /                  | "
-echo " |                        /_/ |_|/___/  /_/  /_/ |_|/___/                  | "
-echo " |                                                                         | "
-echo " |             ___           __                              ____ _        | "
-echo " |            /   |  __  __ / /_ ____   _____ ____   ____   / __/(_)____ _ | "
-echo " |           / /| | / / / // __// __ \ / ___// __ \ / __ \ / /_ / // __ \`/ | "
-echo " |          / ___ |/ /_/ // /_ / /_/ // /__ / /_/ // / / // __// // /_/ /  | "
-echo " |         /_/  |_|\__,_/ \__/ \____/ \___/ \____//_/ /_//_/  /_/ \__, /   | "
-echo " \                                                               /____/    / "
-echo "  -------------------------------------------------------------------------  "
-echo "   \                                                                         "
-echo "    \   \_\_    _/_/                                                         "
-echo "     \      \__/                                                             "
-echo "            (oo)\_______                                                     "
-echo "            (__)\       )\/\                                                 "
-echo "                ||----w |                                                    "
-echo "                ||     ||                                                    "
 
 
 echo "*=========  vimrc     setup  =========*"
@@ -43,4 +59,3 @@ else
   suco cp ./vimrc $virc
 fi
 echo "*=========  vimrc setup end  =========*"
-
